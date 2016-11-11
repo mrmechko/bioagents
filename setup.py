e2d69a85129670a52e5bdbef77959340e41f63c0
Rik Bose <rik@buoy.org>
2016-11-11 18:47:34 -0500

    Bump version: 1.0.1 → 1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 def main():
     setup(name='tripsmodule',
-          version='1.0.1',
+          version='1.0.2',
           description='Python TripsModule',
           long_description='surgically removed from bioagents',
           author='Benjamin Gyori',
