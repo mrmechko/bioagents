f17af751c185a165b137f624af36eacc212ef3a6
Rik <rbose@cs.rochester.edu>
2019-10-28 14:06:32 -0400

    Bump version: 1.1.0 → 1.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 def main():
     setup(name='tripsmodule',
-          version='1.1.0',
+          version='1.1.1',
           description='Python TripsModule',
           long_description='surgically removed from bioagents',
           author='Benjamin Gyori',
